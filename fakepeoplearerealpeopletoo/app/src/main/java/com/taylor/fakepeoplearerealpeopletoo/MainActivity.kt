71a60dc8cb51cb71b2b82ca193614aae9179d0f0
Taylor Wilcox <mtwilcox@verisk.com>
2019-04-22 15:19:06 -0600

    Log Activity Lifecycle methods.

--- fakepeoplearerealpeopletoo/app/src/main/java/com/taylor/fakepeoplearerealpeopletoo/MainActivity.kt
+++ fakepeoplearerealpeopletoo/app/src/main/java/com/taylor/fakepeoplearerealpeopletoo/MainActivity.kt
@@ -2,11 +2,42 @@ package com.taylor.fakepeoplearerealpeopletoo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "Lifecycle"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(TAG, "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy")
     }
 }
